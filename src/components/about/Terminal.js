858a7e181d7fb76b688226d68eec8c16233da92f
Adeel-works <72549306+Adeel-works@users.noreply.github.com>
2023-01-22 21:32:56 +0500

    added projects

--- src/components/about/Terminal.js
+++ src/components/about/Terminal.js
@@ -20,7 +20,7 @@ function Terminal(props) {
          <Box py={{xs: '1rem', md: '2rem'}} px={{xs: '2rem', md: '3rem'}} borderRadius={'0 0 0.5rem 0.5rem'}
               sx={{backgroundColor: '#27242f'}} fontSize={'1.5rem'} fontFamily={'Courier New, Courier, monospace'}>
             {text}
-         </Box>
+         </Box> 
       </Box>
    );
 }
