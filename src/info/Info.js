a55408dc2c6470db4989ed516387f4db03ac4ad2
Adeel-works <72549306+Adeel-works@users.noreply.github.com>
2023-01-22 11:32:25 +0500

    first commit

---
+++ src/info/Info.js
@@ -0,0 +1,117 @@
+import self from "../img/photo.png"
+import mock1 from "../img/mocks.jpg"
+import mock2 from "../img/mock2.png"
+import mock3 from "../img/mock3.png"
+import mock4 from "../img/mock4.png"
+import mock5 from "../img/mock5.png"
+
+/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
+how to make this template work best for you! */
+
+export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+/*
+I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
+These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
+the site.
+ */
+
+
+/*
+So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
+that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
+of what each of the values mean.
+ */
+
+export const info = {
+    firstName: "Adeel",
+    lastName: "Mirza",
+    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    position: "I engineer solutions",
+    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    baseColor: colors[0],
+    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+        {
+            emoji: '☕',
+            text: 'fueled by coffee'
+        },
+        {
+            emoji: "💼",
+            text: "Senior Software Engineer @ Codeninjaconsulting"
+        },
+        {
+            emoji: "📧",
+            text: "techgeek027@gmail.com"
+        }
+    ],
+    socials: [
+        {
+            link: "https://github.com",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        {
+            link: "https://linkedin.com",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        },
+// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+// Just change the links so that they lead to your social profiles.
+
+    ],
+    bio: "Greetings! I'm Adeel. I'm a senior software engineer at codeninja consultings. I studied bscs at ucp, I think there's a fine line between a developer and an engineer, Thats why you should hire me !!",
+    skills:
+        {
+            proficientWith: ['javascript', 'react','react-native','vue','firebase','git', 'github', 'bootstrap', 'html5', 'css3', ],
+            exposedTo: ['nodejs', 'adobe xd', 'docker', 'aws', 'gcp', 'postgresql']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'exercise',
+            emoji: '💪🏽',
+        },
+        {
+            label: 'car enthusiast',
+            emoji: '🏎️',
+        },
+// Same as above, change the emojis to match / relate to your hobbies or interests.
+// You can also remove the emojis if you'd like, I just think they look cute :P
+    ],
+    portfolio: [ // This is where your portfolio projects will be detailed
+        {
+            title: "Graana",
+            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
+        },
+        {
+            title: "Funoonee",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock2
+        },
+        {
+            title: "Project 3",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock3
+        },
+        {
+            title: "Project 4",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock4
+        },
+        {
+            title: "Project 5",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock5
+        }
+    ]
+}
